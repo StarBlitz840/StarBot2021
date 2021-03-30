d790c21f575f507f16fee068aaa653875547484b
simplyCyclops <maipez04@gmail.com>
2021-03-30 15:20:49 +0300

    added gyro follow

--- util/gyro.py
+++ util/gyro.py
@@ -1,5 +1,16 @@
 from robot import Robot
 
 def turn_to(target, speed):
-  Robot.chassis.turn(target - Robot.gyro)
-  
+  Robot.chassis.turn(target - Robot.gyro.angle)
+
+
+
+def follow_angle(target, distance, kp, drive_speed):
+  Robot.chassis.drive(drive_speed, 0)
+
+  while Robot.chassis.distance() < distance:
+    error = target - Robot.gyro.angle()
+    Robot.chassis.drive(0, error * kp)
+
+  Robot.brake()
+    
--- util/line.py
+++ util/line.py
@@ -6,25 +6,24 @@ WHITE = 100
 #1 = right
 #
 def follow_line(is_right_sensor, line_side, distance, kp, drive_speed):
-    target = (BLACK + WHITE) / 2
-    sensor = Robot.color_right
-    
-    if is_right_sensor == False:
-        sensor = Robot.color_left
+  target = (BLACK + WHITE) / 2
+  sensor = Robot.color_right
+  
+  if is_right_sensor == False:
+      sensor = Robot.color_left
 
-    Robot.chassis.reset()
-    Robot.chassis.drive(drive_speed, 0)
+  Robot.chassis.reset()
+  Robot.chassis.drive(drive_speed, 0)
 
-    while Robot.chassis.distance() <= distance:
-        #when error < 0: steers to white.
-        #when error > 0: steers to black.
-        error = target - sensor.reflection()
+  while Robot.chassis.distance() < distance:
+      #when error < 0: steers to white.
+      #when error > 0: steers to black.
+      error = target - sensor.reflection()
 
+      if line_side == "left":
+          Robot.chassis.drive(drive_speed, -error * kp)
+      else:
         #when following or right side
-        Robot.chassis.drive(drive_speed, error)
+        Robot.chassis.drive(drive_speed, error * kp)
 
-        if line_side == "left":
-            Robot.chassis.drive(drive_speed, -error)
-
-
-    Robot.brake()
+  Robot.brake()
