2108a360d5c2b7cea0a4ffc6bc54a4c973378c28
StarBlitz <starblitz.840@gmail.com>
2021-03-30 14:36:03 +0300

    Added follow line function

--- util/line.py
+++ util/line.py
@@ -8,30 +8,23 @@ WHITE = 100
 def follow_line(is_right_sensor, line_side, distance, kp, drive_speed):
     target = (BLACK + WHITE) / 2
     sensor = Robot.color_right
+    
+    if is_right_sensor == False:
+        sensor = Robot.color_left
 
     Robot.chassis.reset()
     Robot.chassis.drive(drive_speed, 0)
-
-    if is_right_sensor == False:
-        sensor = Robot.color_left
 
     while Robot.chassis.distance() <= distance:
         #when error < 0: steers to white.
         #when error > 0: steers to black.
         error = target - sensor.reflection()
 
-        if #סטייה > target:
-            #לתקן -1 * line_side
-        elif #סטייה < target:
-            #לתקן  1 * line_side
+        #when following or right side
+        Robot.chassis.drive(drive_speed, error)
+
+        if line_side == "left":
+            Robot.chassis.drive(drive_speed, -error)
 
 
-
-    #if right side sensor = right sensor
-    #if right side if line mod = -1 (don't do this yet)
-
-    #target = average of black, white
-
-    #while not traveled distance:
-    #find error
-    #do turn rate of kp * error
+    Robot.brake()
